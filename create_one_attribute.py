039fa2ef9a73015a3dc4a6e0ba0f6a0ea3457836
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-24 17:51:37 +0500

    create a Persan class, create attribute name

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,12 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+
+class Person:
+    name = 'person'
+
+    def __init__(self, first: str, last: str, age: int):
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+        self.full_name = f'{self.first_name} {self.last_name}'
+        
