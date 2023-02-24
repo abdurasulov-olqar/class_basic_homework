4750f541b2453a136bc2acf97a804b2d09b49eb1
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-24 17:36:22 +0500

    create a person class

--- person.py
+++ person.py
@@ -1,1 +1,12 @@
 #Create a "Person" class
+class Person:
+    name = "person"
+
+    def __init__(self, first: str, last: str, age: int, gender = "male"):
+        self.first_name = first
+        self.last_name = last
+        self.age = age
+        self.gender = gender
+        self.full_name = f'{self.first_name} {self.last_name}'
+
+print(Person.name)
