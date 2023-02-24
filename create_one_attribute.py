fb934b8369fc58b5c75e33b5d381cd8cbb477990
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-24 18:05:33 +0500

    create a person class

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -2,11 +2,8 @@
 #Create an attribute "name" in the "Person" class
 
 class Person:
-    name = 'person'
-
-    def __init__(self, first: str, last: str, age: int):
-        self.first_name = first
-        self.last_name = last
-        self.age = age
-        self.full_name = f'{self.first_name} {self.last_name}'
+    def __init__(self, name):
+        
+        self.name = name
+       
         
